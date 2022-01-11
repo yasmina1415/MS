bcb54ea978f64b9a97cf37a042fe18c02b3822b1
yasmina6714 <yassminaabdelazim@gmail.com>
2022-01-11 18:43:48 +0200

    initialize the project

---
+++ src/main/java/com/example/myeurekaserver/MyEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.myeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyEurekaServerApplication.class, args);
+	}
+
+}
